c533440c938db9fe18373170708dcd6e2e2c9fb0
winniediana4 <deinawinnie84@gmail.com>
2021-06-14 09:34:35 +0300

    add comment and username

--- main/views.py
+++ main/views.py
@@ -22,6 +22,48 @@ def posts():
   return render_template('pitch_display.html', posts = posts, likes = likes, user = user)
 
 
-  
+@main.route('/new_post', methods = ['GET', 'POST'])
+@login_required
+def new_post():
+  form = PostForm()
+  if form.validate_on_submit():
+    title = form.title.data
+    post = form.post.data
+    category = form.category.data
+    user_id = current_user._get_current_object().id
+    post_obj = post(post = post, title = title, category = category, user_id = user_id)
+    
+    post_obj.save()
+    return redirect(url_for('main.index'))
+  return render_template('pitch.html', form = form)
 
 
+  @main.route('/comment/<int:post_id>', methods = ['GET', 'POST'])
+  @login_required
+  def comment(post_id):
+    form = CommentForm()
+    post = post.query.get(post_id)
+    user = User.query.all()
+    comments = comment.query.filter_by(post_id = post_id).all()
+    if form.validate_on_submit():
+      comment = form.comment.data  
+      post_id = post_id
+      user_id = current_user._get_current_object().id
+      new_comment =comment(comment = comment, post_id = post_id, user_id = user_id)
+
+
+      new_comment.save()
+      new_comments = [new_comment]
+      print(new_comments)
+      return redirect(url_for('.comment', post_id = post_id))
+    return render_template('comment.html', form = form, post = post, comments = comments, user = user)
+
+
+    @main.route('/user')
+    @login_required
+    def user():
+      username = current_user.username
+      user = User.query.filter_by(username = username).first()
+      if user is None:
+        return('not found')
+        return render_template('profile.html', user = user)  
